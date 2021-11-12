2ee4d9c593b8456de80a34ff40fc49d07bf368ee
IamLoWLo <iamlowlo@gmail.com>
2021-11-12 13:29:14 +0100

    feat(Index): add update depencency tools

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require('fs-extra')
+
+const copyFile = (origin, destination) => {
+  fs.copy(origin, destination, function (err) {
+    if (err) {
+      console.error(`An error occured while copying ${origin}.`)
+      return console.error(err)
+    }
+    console.log(`Copy ${origin} completed!`)
+  })
+}
+
+const initWatchers = ({ source, destination, label }) => {
+  console.log(`Watching files on ${label}`)
+  fs.watch(source, { recursive: true }, async (eventType, filename) => {
+    if (eventType !== 'rename' && filename[filename.length - 1] !== '~') {
+      console.log(`${filename}: [${eventType}]`)
+      try {
+        await fs.unlink(`${destination}${filename}`)
+      } catch (e) {
+        console.log(`error on pre-deleting existing file ${destination}${filename}`)
+      }
+      copyFile(`${source}${filename}`, `${destination}${filename}`)
+    }
+  })
+}
+
+const pruneDirectory = async (folder) => {
+  console.log(`Prune Directory: ${folder}`)
+  try {
+    await fs.promises.rmdir(folder, { recursive: true })
+  } catch (e) {
+    console.log(`Error occurred pruning ${folder}`)
+  }
+}
+
+exports.pruneAndCopyAllFilesOnce = (config) => {
+  process.env.NC43_DEPENDENCY_CONFIG = JSON.stringify(config)
+  config.forEach((config) => {
+    pruneDirectory(config.destination).then(() => {
+      copyFile(config.source, config.destination)
+    })
+  })
+}
+exports.watchUIFiles = () => {
+  const config = JSON.parse(process.env.NC43_DEPENDENCY_CONFIG)
+  config.forEach((config) => {
+    initWatchers(config)
+  })
+}
