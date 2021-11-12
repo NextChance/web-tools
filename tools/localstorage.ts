638948bb09c5372570cbe952bb6e6c6efe4ccc15
IamLoWLo <iamlowlo@gmail.com>
2021-11-12 13:10:14 +0100

    feat(LocalStorage): add localStorage tool from B2HandsWeb

---
+++ tools/localstorage.ts
@@ -0,0 +1,43 @@
+interface Data {
+  [key: string]: string
+}
+
+class LocalStorage {
+  data: Data
+
+  constructor () {
+    this.data = JSON.parse(process.env.LOCAL_STORAGE || '{}')
+  }
+
+  get (key: string, defaultValue: string = ''): string {
+    if (window?.localStorage) {
+      return window.localStorage[key] === undefined
+        ? defaultValue
+        : window.localStorage.getItem(key) || ''
+    } else {
+      return !this.data[key] ? defaultValue : this.data[key]
+    }
+  }
+
+  set (key:string, value:string):void {
+    if (window?.localStorage) {
+      window.localStorage.setItem(key, value)
+    } else {
+      this.data[key] = value
+      process.env.LOCAL_STORAGE = JSON.stringify(this.data)
+    }
+  }
+
+  remove (key:string):void {
+    if (window?.localStorage) {
+      window.localStorage.removeItem(key)
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [key]: removedKey, ...remainingData } = this.data
+      this.data = remainingData
+      process.env.LOCAL_STORAGE = JSON.stringify(this.data)
+    }
+  }
+}
+
+export default new LocalStorage()
