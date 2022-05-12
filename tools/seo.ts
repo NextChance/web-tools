8066d7f7e48b83fb2ed8391848c197a3dee7b18f
hgherna <hugo.hernandez@nc43tech.com>
2022-05-12 12:54:23 +0100

    feat(): update seo tool

--- tools/seo.ts
+++ tools/seo.ts
@@ -1,9 +1,11 @@
 export const getMetaTags = ({
   description = undefined,
-  title = undefined
+  title = undefined,
+  image = undefined
 }: {
   description: string | undefined,
-  title: string | undefined
+  title: string | undefined,
+  image?: string | undefined
 }) => {
   return [
     {
@@ -20,6 +22,11 @@ export const getMetaTags = ({
       hid: 'og:description',
       property: 'og:description',
       content: description
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: image
     }
   ]
 }
